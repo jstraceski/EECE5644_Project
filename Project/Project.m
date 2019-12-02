1efd1ee39c44ce79933dcb5289e2342c5ebc51fb
erronknight <erronknight@gmail.com>
2019-12-02 04:28:51 -0500

    testing / training data

--- Project/Project.m
+++ Project/Project.m
@@ -1,25 +1,26 @@
 clf
-% glassesData = readtable('glasses.csv');
-% report = readtable('report.csv');
-% 
-% div = 100;
-% 
-% reduction = ceil(height(glassesData(:, 1))/div);
-% arr = zeros(reduction, 9);
-% 
-% for i = 1:div:height(glassesData(:, 1))
-%     formatIn = 'yyyy/mm/dd HH:MM:SS.FFF';
-%     arr(ceil(i/div), 1) = datenum(glassesData{i, 2},formatIn);
-%     arr(ceil(i/div), 2) = glassesData{i, 3};
-%     arr(ceil(i/div), 3) = glassesData{i, 4};
-%     arr(ceil(i/div), 4) = glassesData{i, 5};
-%     arr(ceil(i/div), 5) = glassesData{i, 6};
-%     arr(ceil(i/div), 6) = glassesData{i, 7};
-%     arr(ceil(i/div), 7) = glassesData{i, 8};
-%     
-%     arr(ceil(i/div), 9) = sqrt(glassesData{i, 3}^2 + glassesData{i, 4}^2 + glassesData{i, 5}^2);
-% end
 
+glassesData = readtable('glasses.csv');
+report = readtable('report.csv');
+% 
+div = 100;
+% 
+reduction = ceil(height(glassesData(:, 1))/div);
+arr = zeros(reduction, 9);
+ 
+for i = 1:div:height(glassesData(:, 1))
+    formatIn = 'yyyy/mm/dd HH:MM:SS.FFF';
+    arr(ceil(i/div), 1) = datenum(glassesData{i, 2},formatIn);
+    arr(ceil(i/div), 2) = glassesData{i, 3};
+    arr(ceil(i/div), 3) = glassesData{i, 4};
+    arr(ceil(i/div), 4) = glassesData{i, 5};
+    arr(ceil(i/div), 5) = glassesData{i, 6};
+    arr(ceil(i/div), 6) = glassesData{i, 7};
+    arr(ceil(i/div), 7) = glassesData{i, 8};
+     
+    arr(ceil(i/div), 9) = sqrt(glassesData{i, 3}^2 + glassesData{i, 4}^2 + glassesData{i, 5}^2);
+end
+%}
 arr(:, 8) = 0;
 
 C = categorical(report{:,2});
@@ -90,7 +91,7 @@ for i = 1:height(report)
     
 end
 
-figure(1)
+figure()
 x = 1:length(arr);
 hold on
 scatter(x(arr(:, 8) == 4), arr(arr(:, 8) == 4, 9), 'black', '.')
@@ -98,3 +99,10 @@ scatter(x(arr(:, 8) == 5), arr(arr(:, 8) == 5, 9), 'yellow', '.')
 scatter(x(arr(:, 8) == 3), arr(arr(:, 8) == 3, 9), 'green', '.')
 scatter(x(arr(:, 8) == 2), arr(arr(:, 8) == 2, 9), 'red', '.')
 scatter(x(arr(:, 8) == 1), arr(arr(:, 8) == 1, 9), 'blue', '.')
+
+figure() % Scatter plot
+x = 1:length(arr);
+hold on
+scatter(arr(arr(:, 8) == 2,4),  arr(arr(:, 8) == 2, 5), 'blue', '.');
+scatter(arr(arr(:, 8) == 3,4), arr(arr(:, 8) == 3, 5), 'red', '.');
+legend('Walk','Meeting/Computer');
